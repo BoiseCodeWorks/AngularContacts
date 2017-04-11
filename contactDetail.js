482a210dbfa9a80b21456db2b7ee8b4ba5848f36
javaday@gmail.com <javaday@gmail.com>
2017-04-10 20:35:25 -0600

    Added edit modal

--- contactDetail.js
+++ contactDetail.js
@@ -10,17 +10,48 @@
 		}
 	});
 
-	ContactDetail.$inject = ['$state', '$stateParams', '$firebaseObject'];
+	app.controller('editContactModalController', EditContactModalController);
 
-	function ContactDetail($state, $stateParams, $firebaseObject) {
+	ContactDetail.$inject = ['$state', '$stateParams', '$firebaseObject', '$uibModal'];
+	EditContactModalController.$inject = ['$uibModalInstance', 'contact'];
+
+	function ContactDetail($state, $stateParams, $firebaseObject, $uibModal) {
 
 		var cd = this;
 		var contactId = $stateParams.id;
 
+		cd.edit = function () {
+			
+			var modal = $uibModal.open({
+				controller: 'editContactModalController',
+				controllerAs: 'emc',
+				templateUrl: 'editContactModal.html',
+				resolve: {
+					contact: function () {
+						return angular.copy(cd.contact);
+					}
+				}
+			});
+
+			modal.result.then(
+				function (changedContact) {
+
+					cd.contact.firstName = changedContact.firstName;
+					cd.contact.lastName = changedContact.lastName;
+					cd.contact.phone = changedContact.phone;
+
+					cd.contact.$save();
+				},
+				function () {
+					// canceled
+				}
+			);
+		};
+		
 		cd.delete = function () {
 			cd.contact.$remove();
 			$state.go('dashboard', { category: cd.category });
-		}
+		};
 
 		cd.$onInit = function () {
 
@@ -36,6 +67,39 @@
 				.catch(function (error) {
 					console.error("Error:", error);
 				});
+		}
+	}
+
+	function EditContactModalController($uibModalInstance, contact) {
+
+		var emc = this;
+		
+		emc.errors = [];
+		emc.contact = contact;
+
+		emc.ok = function () {
+			if (validate()) {
+				$uibModalInstance.close(emc.contact);
+			}	
+		};
+
+		emc.cancel = function () {
+			$uibModalInstance.dismiss();
+		};
+
+		function validate() {
+
+			emc.errors = [];
+			
+			if (!emc.contact.firstName) {
+				emc.errors.push('A first name is required.');
+			}			
+
+			if (!emc.contact.phone) {
+				emc.errors.push('A phone number is required.');
+			}
+			
+			return emc.errors.length === 0;
 		}
 	}
 
